5f70c5956e5b61b44b47cbb13efd62b22dd3ef56
Voronik <voronkina.2018@azure.nstu.ru>
2020-05-14 15:44:57 +0700

    2 task

--- DFT_IDFT/DFT_IDFT/DFT.h
+++ DFT_IDFT/DFT_IDFT/DFT.h
@@ -3,7 +3,6 @@
 #include <iomanip>
 #include <vector>
 #include <complex>
-
 #include "vector.h"
 
 class DiscreteTransformation
@@ -54,7 +53,31 @@ public:
 	}
 	void RealizeIDFT(const std::vector<std::complex<double>>& data, std::vector<std::complex<double>>& result)
 	{
+		std::vector<std::vector<std::complex<double>>> W;
+		int N = data.size();
+		result.clear(); result.resize(N);
+		W.resize(N);
+		for (int i = 0; i < N; i++)
+		{
+			W[i].resize(N);
+			W[i][0] = 1.0;
+			W[0][i] = 1.0;
+		}
+		for (int i = 1; i < N; i++)
+			for (int j = i; j < N; j++)
+			{
+				W[i][j]._Val[0] = W[j][i]._Val[0] = cos(-2.0 * PI * i * j / N);
+				W[i][j]._Val[1] = W[j][i]._Val[1] = -sin(-2.0 * PI * i * j / N);
+			}
 
+		for (int i = 0; i < N; i++)
+		{
+			result[i]._Val[0] = 0.0;
+			result[i]._Val[1] = 0.0;
+			for (int j = 0; j < N; j++)
+				result[i] += data[j] * W[i][j];
+			result[i] /= double(N);
+		}
 	}
 	void RealizeIFFT(const std::vector<std::complex<double>>& data, std::vector<std::complex<double>>& result)
 	{
--- DFT_IDFT/DFT_IDFT/Source.cpp
+++ DFT_IDFT/DFT_IDFT/Source.cpp
@@ -1,9 +1,9 @@
 #include "vector.h"
 #include "DFT.h"
-
+#include <chrono>
 int main()
 {
-	VectorZ vector(256);
+	VectorZ vector(512);
 	DiscreteTransformation DFT;
 	vector.type = cos2pwj;
 	vector.w = 100;
@@ -12,20 +12,29 @@ int main()
 	vector.fi = -PI;
 	vector.FillVector();
 	
+	//auto start = std::chrono::high_resolution_clock::now();
+
 	DFT.RealizeDFT(vector.Z, vector.DFT_Data);
+	//auto end = std::chrono::high_resolution_clock::now();
+	//std::chrono::duration<double> diff = end - start;
+	//std::cout << "Time DFT " << diff.count() << std::endl;
+
+	//auto start1 = std::chrono::high_resolution_clock::now();
+
 	DFT.RealizeFFT(vector.Z, vector.FFT_Data);
+	
+	//auto end1 = std::chrono::high_resolution_clock::now();
+	//std::chrono::duration<double> diff1 = end1 - start1;
+	//std::cout << "Time FFT" << diff1.count() << std::endl;
+
 
 	for (int i = 0; i < vector.FFT_Data.size(); i++)
 	{
-		std::cout << vector.FFT_Data[i]._Val[0] << "+ " << vector.FFT_Data[i]._Val[1] << "i\n";
+		std::cout << vector.Z[i]._Val[0] << "    " <<vector.FFT_Data[i]._Val[0] << "    " << vector.FFT_Data[i]._Val[1] << "i\n";
 	}
-	std::cout <<"\n";
-	std::cout <<"\n";
-	std::cout <<"\n";
-	std::cout <<"\n";
 
-	for (int i = 0; i < vector.DFT_Data.size(); i++)
-	{
-		std::cout << vector.DFT_Data[i]._Val[0] << "+ " << vector.DFT_Data[i]._Val[1] << "i\n";
-	}
+	//for (int i = 0; i < vector.DFT_Data.size(); i++)
+	//{
+	//	std::cout << vector.Z[i]._Val[0] << "    " << vector.DFT_Data[i]._Val[0] << "+ " << vector.DFT_Data[i]._Val[1] << "i\n";
+	//}
 }
